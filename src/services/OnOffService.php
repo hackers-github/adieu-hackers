fc202e1ed242fa83163c6c9eef583450d7ae3c4a
kksskk2 <doerkskdev@gmail.com>
2024-11-18 16:41:28 +0900

    로그인 페이지

--- src/services/OnOffService.php
+++ src/services/OnOffService.php
@@ -16,6 +16,8 @@ class OnOffService
     // 관리자 투표 종료 체크
     public function checkOnOff($onOff) {
         if($onOff == '2' && $_SESSION['hackers2024_member_user_level'] == '1'){
+            session_unset();
+            session_destroy();
             return ['result' => 'fail', 'message' => '종료된 투표입니다.'];
         } else {
             return ['result' => 'success', 'message' => '투표 진행중입니다.'];
