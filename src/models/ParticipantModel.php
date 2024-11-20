bf407ecb86a1ff7727d93d3c3e3cdd999d26e8ec
kksskk2 <doerkskdev@gmail.com>
2024-11-20 10:50:11 +0900

    투표 설정 페이지

--- src/models/ParticipantModel.php
+++ src/models/ParticipantModel.php
@@ -7,7 +7,7 @@ use src\models\BaseModel;
 class ParticipantModel extends BaseModel
 {
     public function selectParticipantList() {
-        $qry = "SELECT p_id, team_name, title, image_url FROM event_hackers_participant";
+        $qry = "SELECT p_id, team_name, title, image_url, image_org_name FROM event_hackers_participant";
         $this->db_slave->prepare($qry);
         $result = $this->db_slave->stmt_execute('all');
         return $result;
